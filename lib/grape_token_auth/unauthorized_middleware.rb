c171c98bb6101f8466344fce2c2d90d9a1256acb
Michael Cordell <surpher@gmail.com>
2015-09-07 21:30:46 -0700

    Fix unauthorized middleware error message return
    Error messages needed to be an array, not a string.

--- lib/grape_token_auth/unauthorized_middleware.rb
+++ lib/grape_token_auth/unauthorized_middleware.rb
@@ -2,7 +2,7 @@ module GrapeTokenAuth
   class UnauthorizedMiddleware
     def self.call(env)
       errors = env.fetch('warden.options', {})['errors']
-      response = errors ? { 'errors' => errors }.to_json : []
+      response = errors ? [{ 'errors' => errors }.to_json] : []
       [401,
        { 'Content-Type' => 'application/json'
        },
