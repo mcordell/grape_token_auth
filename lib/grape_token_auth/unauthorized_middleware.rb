adaa58e70888a1486469711017a27ea258b0b2f9
Michael Cordell <surpher@gmail.com>
2015-09-08 08:26:22 -0700

    Modify unauthorized endpoint for better errors
    Error message setting is inflexible when the error was 401 because of
    the way warden intercepts the request. This commit changes the
    middleware to better handle a warden error message.

--- lib/grape_token_auth/unauthorized_middleware.rb
+++ lib/grape_token_auth/unauthorized_middleware.rb
@@ -1,13 +1,20 @@
 module GrapeTokenAuth
   class UnauthorizedMiddleware
     def self.call(env)
-      errors = env.fetch('warden.options', {})['errors']
-      response = errors ? [{ 'errors' => errors }.to_json] : []
+      warden_opts = env.fetch('warden.options', {})
+      errors = warden_opts['errors'] || warden_opts[:errors]
       [401,
        { 'Content-Type' => 'application/json'
        },
-       response
+       prepare_errors(errors)
       ]
+    end
+
+    def self.prepare_errors(errors)
+      return [] unless errors
+      return [errors.to_json] if errors.class == Hash
+      return [{ 'errors' => errors }.to_json] if errors.class == String
+      []
     end
   end
 end
